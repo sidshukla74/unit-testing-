f85a9fa2758c14bfc40449581c2c0341d5358d25
sidshukla74 <108859004+sidshukla74@users.noreply.github.com>
2022-07-08 14:52:47 +0530

    Add files via upload

---
+++ UnitTesting/main.py
@@ -0,0 +1,9 @@
+#this is the file that we use for checking the functionality of our code
+
+def add(first_val,  second_val):
+    ad_val =  first_val+second_val
+    return ad_val
+
+def sub(first_val, second_val):
+    sub_val = first_val - second_val
+    return sub_val
---
+++ UnitTesting/test.py
@@ -0,0 +1,26 @@
+from msilib.schema import Class
+import unittest
+import main
+
+#we are inheritng from unittest
+class Problem_Check(unittest.TestCase):
+
+
+    def test_add(self):
+        val1 = 10
+        val2 = 20
+        result = main.add(val1, val2)
+        expected = 30
+        self.assertEqual(result, expected)
+
+    def test_sub(self):
+        val1 = 20
+        val2 = 10
+        result = main.sub(val1, val2)
+        expected = 10
+        self.assertEqual(result, expected)
+        
+
+if __name__ == "__main__":
+    unittest.main()
+
