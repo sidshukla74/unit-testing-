781a1ba89946a0c42eabf81f5e1f5f84a9c6e133
shuklasid19 <shuklasid19@gmail.com>
2022-09-17 16:59:03 +0530

    function added

--- UnitTesting/main.py
+++ UnitTesting/main.py
@@ -12,7 +12,7 @@ def sub(first_val, second_val):
 def divide(first_val ,second_val):
     div_val = first_val / second_val
     return div_val
-
+"""
 def multiply(first_val  , second_val):
     multp_val = first_val * second_val
-    return multp_val
+    return multp_val"""
